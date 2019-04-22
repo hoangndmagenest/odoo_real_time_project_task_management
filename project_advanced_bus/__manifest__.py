e6c6f38a1b682e1b7cbf45e856806e99511713d0
logan <odoo@hungnamecommerce.com>
2019-04-22 11:30:35 +0700

    update screenshot

--- project_advanced_bus/__manifest__.py
+++ project_advanced_bus/__manifest__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Real Time Project Task Management",
+    'name': "Real Time Projects and Tasks Management",
 
     'summary': """
         All tasks will update realtime
         """,
 
     'description': """
-        Just install/upgrade do not need configuration
+        Just install/upgrade do not need configuration.
         All tasks will update realtime.
         When any project user change a task stage, all users are looking at same project will see new change.
     """,
-
+    'images': ['static/description/main_screenshot.png'],
     'author': "Magenest",
     'website': "https://store.magenest.com",
     'category': 'Project',
